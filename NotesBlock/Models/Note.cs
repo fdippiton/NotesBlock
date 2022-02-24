051cd44611c35d7fc191c9bbf74aea4211cdba5e
Frandipfer <francinagio@hotmail.com>
2022-02-24 15:53:02 -0400

    Add project files.

---
+++ NotesBlock/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿namespace NotesBlock.Models
+{
+    public class Note
+    {
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private string Author { get; set; }
+        private string Date { get; set; }
+
+        public Note()
+        {
+
+        }
+
+
+    }
+}
